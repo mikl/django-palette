e82544d23891781f1713f2aa2f590ba8384536a2
Mikkel Hoegh <mikkel@hoegh.org>
2009-08-05 13:26:18 +0200

    Made the display of palettes work.

--- demo/site_media/js/palette.js
+++ demo/site_media/js/palette.js
@@ -3,11 +3,11 @@
 $(function () {
     $(".palette_list .colour_box").each(function (i) {
         var box = $(this);
-        var count = box.length;
+        var count = box.children().length;
 
         box.children().each(function (i) {
             $(this)
-              .width(box.width() / count)
+              .width(box.width()/count)
               .css("background-color", $(this).text())
               .html('&thinsp;');
         });
